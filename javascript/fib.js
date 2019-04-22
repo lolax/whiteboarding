51e3f6d783a6bee60f09db436bc7dbc0b19669ba
Lola <lolahef@gmail.com>
2019-04-22 14:18:07 -0400

    Finished fib in js

---
+++ javascript/fib.js
@@ -0,0 +1,17 @@
+const fib = n => {
+
+    let cache = {
+        0: 0,
+        1: 1
+    }
+    
+
+    for (i = 2; i < n; i ++) {
+        cache[i] = cache[i - 1] + cache[i - 2]
+        console.log(cache[i])
+    }
+    
+    return cache
+}
+
+console.log(fib(30))
