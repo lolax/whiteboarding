0dbe35df9154833cc314f42d0f19c9d383df8938
Lola <lolahef@gmail.com>
2019-04-26 17:25:48 -0400

    Singly linked class created; needs methods

---
+++ javascript/linkedList.js
@@ -0,0 +1,17 @@
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const a = new ListNode('a');
+const b = new ListNode('b');
+const c = new ListNode('c');
+const d = new ListNode('d');
+const e = new ListNode('e');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
