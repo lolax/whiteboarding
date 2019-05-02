77e733fd9c5bd4b22196ab3ceaa8ce38b48fe45b
Lola <lolahef@gmail.com>
2019-05-02 12:46:23 -0400

    Finished wordCloud

---
+++ javascript/wordCloud.js
@@ -0,0 +1,12 @@
+const wordCloud = str => {
+    str = str.split('').map(letter => letter.toLowerCase()).join('')
+    let words = str.split(' ')
+    let wordMap = new Map()
+    words.forEach(word => {
+        wordMap.has(word) ? wordMap.set(word, wordMap.get(word) + 1) : wordMap.set(word, 1)
+    })
+}
+
+const testString = "HI yes Hello yes yes hi"
+
+wordCloud(testString)
