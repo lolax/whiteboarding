4116f5881b702ee3f13ce7d05d43bf8c0739f9a5
Lola <lolahef@gmail.com>
2019-05-27 14:11:58 -0400

    Finished making change

---
+++ javascript/makingChange.js
@@ -0,0 +1,35 @@
+const recursiveMakingChange = (amount, denom, index=0, cache={}) => {
+    
+    const cacheKey = `${amount}-${index}`
+    if (cache.hasOwnProperty(cacheKey)) return cache[cacheKey]
+    if (amount === 0) return 1 
+    if (amount < 0 || index === denom.length) return 0
+
+    const currentCoin = denom[index]
+    let ways = 0
+    
+    while (amount >= 0) {
+        ways += recursiveMakingChange(amount, denom, index + 1, cache)
+        amount -= currentCoin
+    }
+
+    cache[cacheKey] = ways
+    return ways
+}
+
+const bottomUpMakingChange = (amount, denom) => {
+    const ways = new Array(amount + 1).fill(0)
+    ways[0] = 1
+
+    denom.forEach(coin => {
+        for (let higherAmount = coin; higherAmount <= amount; higherAmount++) {
+            const remainder = higherAmount - coin
+            ways[higherAmount] += ways[remainder]
+        }
+    })
+    return ways[amount]
+}
+// if (cache[`${amount}-${coins}`]) return cache[`${amount}-${coins}`]
+
+console.log(recursiveMakingChange(5, [1, 5, 10, 25, 50]))
+console.log(bottomUpMakingChange(5, [1, 5, 10, 25, 50]))
